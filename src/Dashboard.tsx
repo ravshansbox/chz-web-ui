3a3b3a11fb112294b8aa02dbbeadf2013a25316b
Ravshan Samandarov <ravshansbox@gmail.com>
2023-01-29 22:18:51 +0500

    add sign in styles

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from './auth';
+
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  const auth = useAuth();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated && !auth.isAuthenticating) {
+      navigate('/signin');
+    }
+  }, []);
+
   return <h1>Dashboard</h1>;
 };
--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,3 +1,21 @@
+import classes from './SignIn.module.scss';
+
 export const SignIn = () => {
-  return <h1>Sign in</h1>;
+  return (
+    <div className={classes.container}>
+      <form className={classes.form}>
+        <label>
+          <span>Username</span>
+          <input className={classes.input} />
+        </label>
+        <label>
+          <span>Password</span>
+          <input className={classes.input} />
+        </label>
+        <button className={classes.button} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
