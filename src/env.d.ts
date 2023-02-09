1acb531330f4b7c998b40442f9a427602e2589dc
Ravshan Samandarov <ravshansbox@gmail.com>
2023-02-09 23:20:03 +0500

    remove VITE_COMMIT_HASH

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,2 @@
 declare const VITE_APP_VERSION: string;
 declare const VITE_BUILD_TIMESTAMP: string;
-declare const VITE_COMMIT_HASH: string;
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import react from '@vitejs/plugin-react';
-import { execSync } from 'node:child_process';
 
 /** @type {import('vite').UserConfig} */
 const config = {
   define: {
     VITE_APP_VERSION: JSON.stringify(process.env.npm_package_version),
     VITE_BUILD_TIMESTAMP: JSON.stringify(new Date().toISOString()),
-    VITE_COMMIT_HASH: JSON.stringify(execSync('git rev-parse --short HEAD').toString()),
   },
   plugins: [react()],
   server: {
