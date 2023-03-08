9b9a044a2ae8014bbc63abf9fec32842bf125040
Ravshan Samandarov <ravshansbox@gmail.com>
2023-03-08 17:05:35 +0300

    chore: Show timestamp in local timezone

--- src/utils/logBuildInfo.ts
+++ src/utils/logBuildInfo.ts
@@ -1,4 +1,7 @@
 export const logBuildInfo = () => {
   console.info(`version: %c${VITE_APP_VERSION}`, 'font-weight:bold');
-  console.info(`timestamp: %c${VITE_BUILD_TIMESTAMP}`, 'font-weight:bold');
+  console.info(
+    `timestamp: %c${new Date(VITE_BUILD_TIMESTAMP).toLocaleString()}`,
+    'font-weight:bold',
+  );
 };
