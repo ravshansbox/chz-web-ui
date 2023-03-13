353d69401f2c436b496319e7e5395131933d9e65
Ravshan Samandarov <ravshansbox@gmail.com>
2023-03-13 13:18:16 +0300

    show SelectCompany to only non root users

--- src/components/SelectCompany.tsx
+++ src/components/SelectCompany.tsx
@@ -1,18 +1,22 @@
 import { type ComponentType } from 'react';
 import { useQuery } from 'react-query';
 import { useAppStore } from '../common/appStore';
+import { useAuth } from '../common/auth';
 import { httpClient } from '../common/httpClient';
 import { type Permission } from '../common/types';
 import { Select } from './Select';
 
 export const SelectCompany: ComponentType = () => {
+  const auth = useAuth();
+  const isNormalUser = auth.user !== null && !auth.user.is_root;
   const permissions = useQuery({
     queryKey: ['permissions'],
     queryFn: () => httpClient.fetch<Permission[]>('/permissions'),
+    enabled: isNormalUser,
   });
   const appStore = useAppStore();
 
-  if (!permissions.data) {
+  if (!isNormalUser || !permissions.data) {
     return null;
   }
 
