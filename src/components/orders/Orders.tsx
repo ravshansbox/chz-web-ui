2e3c75a2fc9230f6677b5be77071797998207995
Ravshan Samandarov <ravshansbox@gmail.com>
2023-03-13 17:48:58 +0300

    use correct NavLink component

--- src/components/orders/Orders.tsx
+++ src/components/orders/Orders.tsx
@@ -1,11 +1,12 @@
 import { type ComponentType } from 'react';
 import { useQuery } from 'react-query';
-import { NavLink, useOutlet } from 'react-router-dom';
+import { useOutlet } from 'react-router-dom';
 import { useAppStore } from '../../common/appStore';
 import { httpClient } from '../../common/httpClient';
 import { type Order } from '../../common/types';
 import { Card } from '../core/Card';
 import { List } from '../core/List';
+import { NavLink } from '../core/NavLink';
 import { TableList } from '../core/TableList';
 
 export const Orders: ComponentType = () => {
